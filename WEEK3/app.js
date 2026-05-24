e3edbc57ab98c149a86e45ee0135fea0377afc02
CoderKing498 <yerlenisarrieta@gmail.com>
2026-05-23 21:57:52 -0500

    WEEK 3 TASK 3 AND JSON.stringly

--- WEEK3/app.js
+++ WEEK3/app.js
@@ -1,17 +1,40 @@
-let song = document.getElementById('song')
-let btn = document.getElementById('btnAgregar')
-let playList = document.getElementById('wowPlaylist')
+let song = document.getElementById('song');
+let btn = document.getElementById('btnAgregar');
+let playList = document.getElementById('wowPlaylist');
+
+function getSongs(){
+    return JSON.parse(localStorage.getItem('songs')) || [];
+}
+
+function saveSongs(songs){
+    localStorage.setItem('songs', JSON.stringify(songs));
+}
+console.log(getSongs());
 
 btn.addEventListener("click", (e) => {
-    e.preventDefault()
-    const play = document.createElement("p")
-    play.textContent = song.value
+    e.preventDefault();
+    
+    // Get current songs array
+    let songs = getSongs();
 
+    // Add songs to array
+    songs.push(song.value);
 
-    playList.appendChild(play)
+    saveSongs(songs); 
 
-    song.value = ''
+    // New list item
+    const play = document.createElement("p");
+    
+    // Add song text
+    play.textContent = song.value;
+
+    window.localStorage.setItem('song', song.value);
+
+    // Add item to playlist
+    playList.appendChild(play);
+
+    // Clear input field
+    song.value = '';
 })
 
-console.log(song)
-console.log(playList)
+console.log(getSongs());
