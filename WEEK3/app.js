546f7a734e9581bc11149ec06fd379f946332277
CoderKing498 <yerlenisarrieta@gmail.com>
2026-05-28 19:28:38 -0500

    FINISH WEEK 3: add tipography, test LocalStorage add images, and rest of the tasks

--- WEEK3/app.js
+++ WEEK3/app.js
@@ -2,39 +2,74 @@ let song = document.getElementById('song');
 let btn = document.getElementById('btnAgregar');
 let playList = document.getElementById('wowPlaylist');
 
-function getSongs(){
+function getSongs() {
     return JSON.parse(localStorage.getItem('songs')) || [];
 }
 
-function saveSongs(songs){
+function saveSongs(songs) {
     localStorage.setItem('songs', JSON.stringify(songs));
 }
-console.log(getSongs());
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-    
-    // Get current songs array
-    let songs = getSongs();
 
-    // Add songs to array
-    songs.push(song.value);
+    const textSong = song.value.trim();
 
-    saveSongs(songs); 
+    if (textSong === '') {
+        alert("¿No agregarás ninguna canción?");
+        return;
+    }
 
-    // New list item
-    const play = document.createElement("p");
-    
-    // Add song text
-    play.textContent = song.value;
+    const savedSongs = getSongs();
+    savedSongs.push(textSong);
 
-    window.localStorage.setItem('song', song.value);
+    saveSongs(savedSongs);
+    showPlayList();
 
-    // Add item to playlist
-    playList.appendChild(play);
-
-    // Clear input field
     song.value = '';
-})
+});
 
-console.log(getSongs());
+function showPlayList() {
+
+    let valores = getSongs();
+
+    // LIMPIAR LISTA
+    playList.innerHTML = '';
+
+    if (valores.length === 0) {
+        playList.style.display = 'none';
+        return;
+    }
+
+    playList.style.display = 'flex';
+
+    valores.forEach((valor, indice) => {
+
+        let li = document.createElement('li');
+
+        li.classList.add('song-item');
+
+        li.innerHTML = `
+            <div class="songs-content">
+                <p>${valor}</p>
+                <button class="btn-eliminar">Eliminar</button>
+            </div>
+        `;
+
+        let btnEliminar = li.querySelector('.btn-eliminar');
+
+        btnEliminar.addEventListener('click', function () {
+
+            const updatedSongs = valores.filter((_, i) => {
+                return i !== indice;
+            });
+
+            saveSongs(updatedSongs);
+            showPlayList();
+        });
+
+        playList.appendChild(li);
+    });
+}
+
+showPlayList();
